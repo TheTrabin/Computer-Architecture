b49848de8cfab10b2fdcb2a32017e7441fabd35f
TheTrabin <beyondtears@gmail.com>
2020-12-07 18:10:38 -0800

    Managed to get it to function, but I'm having a hard time pointing to the file unless I drag it into the root with the ls8.py file. Otherwise, it functions as expected.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -25,38 +25,52 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.running = True
+        self.halted = False
         self.pc = 0
         self.ir = 0
-        self.running = True
-        self.ram = [0] * 256
+
         self.reg = [0] * 8
+        self.registers = [0] * 8
         self.reg[7] = 0xF4
+        self.registers[7] = 0xF4
+
+        self.ram = [0] * 256
         
     def ram_read(self, address):
         return self.ram[address]
 
-    def ram_write(self, address, val):
-        self.ram[address] = val
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
-
+        program = []
         # For now, we've just hardcoded a program:
 
         # 0b is calling the action, meaning "this is code for 0-level base"
         # 8 bit operator is the code for the particular program
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000, # R0 - Register 0
-            0b00001000, # Value
-            0b01000111, # PRN R0 - Print Registar 0
-            0b00000000, # R0 - Register 0
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # # From print8.ls8
+        #     # 0b10000010, # LDI R0,8
+        #     # 0b00000000, # R0 - Register 0
+        #     # 0b00001000, # Value
+        #     # 0b01000111, # PRN R0 - Print Registar 0
+        #     # 0b00000000, # R0 - Register 0
+        #     # 0b00000001, # HLT
+        # ]
+        with open(filename) as f:
+            for line in f:
+                comment_split = line.split("#")
+                possible_binary = comment_split[0]
 
+                try:
+                    x = int(possible_binary, 2)
+                    program.append(x)
+                except:
+                    continue
         # position in program. Program is 'memory'
         # if this, then that
 
@@ -70,7 +84,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -98,20 +113,51 @@ class CPU:
         """Run the CPU."""
         self.trace()
 
-        while self.running == True:
-            comm = self.ram[self.pc]
-            operand_a = self.ram[self.pc + 1]
-            operand_b = self.ram[self.pc + 2]
+        # while self.running == True:
+        #     comm = self.ram[self.pc]
+        #     operand_a = self.ram[self.pc + 1]
+        #     operand_b = self.ram[self.pc + 2]
 
 
-            if comm == HLT:
-                self.running = False
-                self.pc += 1
-            elif comm == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
-            elif comm == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            else:
-                print(f'Command not found. Please try again.')
+        #     if comm == HLT:
+        #         self.running = False
+        #         self.pc += 1
+        #     elif comm == PRN:
+        #         print(self.reg[operand_a])
+        #         self.pc += 2
+        #     elif comm == LDI:
+        #         self.reg[operand_a] = operand_b
+        #         self.pc += 3
+        #     elif comm == MUL:
+        #         self.alu(comm, operand_a, operand_b)
+        #         self.pc += self.num_of_ops(comm)
+        #     else:
+        #         print(f'Command not found. Please try again.')
+
+    
+
+        while not self.halted:
+            ir = self.ram_read(self.pc)
+            instruction_to_execute = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            self.execute_instruction(instruction_to_execute, operand_a, operand_b)
+
+    def execute_instruction(self, instruction, operand_a, operand_b):
+        if instruction == HLT:
+            self.halted = True
+            self.pc += self.num_of_ops(instruction)
+        if instruction == PRN:
+            print(self.registers[operand_a])
+            self.pc += self.num_of_ops(instruction)
+        elif instruction == LDI:
+            self.registers[operand_a] = operand_b
+            self.pc += self.num_of_ops(instruction)
+        elif instruction == MUL:
+            self.alu(instruction, operand_a, operand_b)
+            self.pc += self.num_of_ops(instruction)
+        else:
+            print("idk what to do.")
+    
+    def num_of_ops(self, comm):
+        return ((comm >> 6) & 0b11) + 1
